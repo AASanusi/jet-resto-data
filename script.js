3995c88f7a7551702154c7cbc01f757a868fae86
AASanusi <alaminm.sanusi@gmail.com>
2024-02-29 15:50:35 +0000

    Finish writing code to fetch data from API

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 fetch("https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/EC4M7RF")
-    .then(res => console.log(res))
+    .then(response => {
+        if(response.ok) {
+            const data = response.json()
+        } else {
+            throw new Error("Data could not be fetched")
+        }
+    })
+    .then(data => console.log('Response data', data))
     .catch(error => console.log(error))
