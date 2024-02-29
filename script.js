da330c29c7bcb2c66b4ae8809657e8a8bc7531ca
AASanusi <alaminm.sanusi@gmail.com>
2024-02-29 13:43:50 +0000

    Call fetch function to try and access API

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-console.log("Hello")
+fetch("https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/EC4M7RF")
+    .then(res => console.log(res))
+    .catch(error => console.log(error))
