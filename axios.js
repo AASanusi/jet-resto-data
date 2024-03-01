cae2109ac6168b83200ab648e45e82b766ade2b3
AASanusi <alaminm.sanusi@gmail.com>
2024-03-01 15:44:05 +0000

    Console log secondKey to the restaurant object

--- axios.js
+++ axios.js
@@ -3,23 +3,36 @@ const axios = require('axios');
 axios.get("https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/EC4M7RF")
 
   .then(response => {
+
     const data = response.data
     const keys = Object.keys(data)
+
     if (keys.length > 1) {
         const secondKey = keys[1];
-        const restaurants = data[secondKey];
+        // const restaurants = data[secondKey];
+        console.log(secondKey);
+
+        
        
-        console.log("Restaurant name: ", restaurants[0].name)
-        console.log("Address: ", restaurants[0].address)
-        // console.log(`First key: ${firstKey}, Value:`, firstObject);
+        // console.log("Restaurant name: ", restaurants[0].name)
+        // console.log("Address: ", restaurants[0].address)
+        
+
       } else {
         console.log('The response does not contain any keys.');
       }
     })
+
     .catch(error => {
       console.error('Error fetching data:', error);
     });
   
+
+
+
+
+
+
 
 // async function fetchDataFromExternalURL() {
 //     try {
