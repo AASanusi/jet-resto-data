b77b82af73c43fc6f301c1745dc37919077a0d9a
AASanusi <alaminm.sanusi@gmail.com>
2024-03-01 21:59:05 +0000

    Add if statement to determine the success of response based on status code

--- axios.js
+++ axios.js
@@ -1,39 +1,67 @@
+// const axios = require('axios');
+
+// axios.get("https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/EC4M7RF")
+
+//     .then(response => {
+
+//         const data = response.data
+//         const keys = Object.keys(data)
+
+//         if (keys) {
+//             const secondKey = keys[1];
+//             const restaurants = data[secondKey];
+
+
+
+//             for (let i = 0; i < 10; i++) {
+//                 console.log("Restaurant name: ", restaurants[i].name)
+//                 console.log("Cuisines: ", restaurants[i].cuisines)
+//                 console.log("Ratings: ", restaurants[i].rating.starRating)
+//                 console.log("Address: ", restaurants[i].address)
+//                 console.log("---------------------------------------------------------------------")
+//             }
+
+
+//         } else {
+//             console.log('The response does not contain any keys.');
+//         }
+//     })
+
+//     .catch(error => {
+//         console.error('Error fetching data:', error);
+//     });
+
+
 const axios = require('axios');
 
-axios.get("https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/EC4M7RF")
+axios.get("https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/lll")
 
     .then(response => {
 
-        const data = response.data
-        const keys = Object.keys(data)
+        if (response.status === 200) {
 
-        if (keys) {
+            const data = response.data
+            const keys = Object.keys(data)
+            
             const secondKey = keys[1];
             const restaurants = data[secondKey];
+            console.log(restaurants)
 
-
-
-            for (let i = 0; i < 10; i++) {
-                console.log("Restaurant name: ", restaurants[i].name)
-                console.log("Cuisines: ", restaurants[i].cuisines)
-                console.log("Ratings: ", restaurants[i].rating.starRating)
-                console.log("Address: ", restaurants[i].address)
-                console.log("---------------------------------------------------------------------")
-            }
-
-
+            // for (let i = 0; i < 10; i++) {
+            //     console.log("Restaurant name: ", restaurants[i].name)
+            //     console.log("Cuisines: ", restaurants[i].cuisines)
+            //     console.log("Ratings: ", restaurants[i].rating.starRating)
+            //     console.log("Address: ", restaurants[i].address)
+            //     console.log("---------------------------------------------------------------------")
+            // }
         } else {
-            console.log('The response does not contain any keys.');
+            console.log('Error:', response.status);
         }
     })
 
     .catch(error => {
         console.error('Error fetching data:', error);
     });
-
-
-
-
 
 
 
