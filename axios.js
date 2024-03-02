9e410f0e3aefce9ae70c94bd4fcb9216d8f7e85a
AASanusi <alaminm.sanusi@gmail.com>
2024-03-02 17:49:29 +0000

    Insert code for serving static files and listening for response

--- axios.js
+++ axios.js
@@ -1,38 +1,15 @@
-// const axios = require('axios');
-
-// axios.get("https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/EC4M7RF")
-
-//     .then(response => {
-
-//         const data = response.data
-//         const keys = Object.keys(data)
-
-//         if (keys) {
-//             const secondKey = keys[1];
-//             const restaurants = data[secondKey];
-
-
-
-//             for (let i = 0; i < 10; i++) {
-//                 console.log("Restaurant name: ", restaurants[i].name)
-//                 console.log("Cuisines: ", restaurants[i].cuisines)
-//                 console.log("Ratings: ", restaurants[i].rating.starRating)
-//                 console.log("Address: ", restaurants[i].address)
-//                 console.log("---------------------------------------------------------------------")
-//             }
-
-
-//         } else {
-//             console.log('The response does not contain any keys.');
-//         }
-//     })
-
-//     .catch(error => {
-//         console.error('Error fetching data:', error);
-//     });
-
-
 const axios = require('axios');
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  });
+
 
 axios.get("https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/EC4M7RF")
 
