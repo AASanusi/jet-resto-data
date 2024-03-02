d2c26defb4b147ddb4eb19dc9da862ba08bb7782
AASanusi <alaminm.sanusi@gmail.com>
2024-03-02 10:45:20 +0000

    Edit code to check status

--- axios.js
+++ axios.js
@@ -34,45 +34,39 @@
 
 const axios = require('axios');
 
-axios.get("https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/lol")
+axios.get("https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/EC4M7RF")
 
     .then(response => {
 
-        const data = response.data
-        const keys = Object.keys(data)
+        if (response.status === 200) {
+            const data = response.data
+            const keys = Object.keys(data)
 
-        const secondKey = keys[1];
-        const restaurants = data[secondKey];
+            const secondKey = keys[1];
+            const restaurants = data[secondKey];
 
-        if (restaurants.length > 0) {
+            if (restaurants.length > 0) {
 
-            for (let i = 0; i < 10; i++) {
-                console.log("Restaurant name: ", restaurants[i].name)
-                console.log("Cuisines: ", restaurants[i].cuisines)
-                console.log("Ratings: ", restaurants[i].rating.starRating)
-                console.log("Address: ", restaurants[i].address)
-                console.log("---------------------------------------------------------------------")
+                for (let i = 0; i < 10; i++) {
+                    console.log("Restaurant name: ", restaurants[i].name)
+                    console.log("Cuisines: ", restaurants[i].cuisines)
+                    console.log("Ratings: ", restaurants[i].rating.starRating)
+                    console.log("Address: ", restaurants[i].address)
+                    console.log("---------------------------------------------------------------------")
+                }
+            } else {
+                console.log('There are no results found');
             }
+
         } else {
-            console.log('There are no results found');
+            console.log(`Failed to fetch data. Status: ${response.status}`)
         }
+
     })
 
     .catch(error => {
-        console.error('Error fetching data:', error);
+        console.error('Error fetching data:', error.message);
     });
 
 
-
-
-// async function fetchDataFromExternalURL() {
-//     try {
-//         const response = await axios.get("https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/EC4M7RF");
-//     }
-//     catch {
-//         console.error('Error fetching data:', error.message)
-//     }
-// }
-
-// fetchDataFromExternalURL();
 
