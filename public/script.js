5f505180dac83861538ef61afa864036fbecbf6d
AASanusi <alaminm.sanusi@gmail.com>
2024-03-02 18:31:46 +0000

    Testing local express on a browser

--- public/script.js
+++ public/script.js
@@ -1,10 +1,1 @@
-// fetch("https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/EC4M7RF")
-//     .then(response => {
-//         if(response.ok) {
-//             const data = response.json()
-//         } else {
-//             throw new Error("Data could not be fetched")
-//         }
-//     })
-//     .then(data => console.log('Response data', data))
-//     .catch(error => console.log(error))
+console.log('client-side js working')
