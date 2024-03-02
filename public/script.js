9a37dcf200b89074b45fdc5ce922573b76eeb288
AASanusi <alaminm.sanusi@gmail.com>
2024-03-02 20:36:12 +0000

    Structure code

--- public/script.js
+++ public/script.js
@@ -1,14 +1,12 @@
 console.log('client-side js working');
 
 async function getRestaurantData() {
-
     try {
-
         const response = await fetch("/restaurantdata");
 
         if (response.ok) {
             // Parse the response body as JSON
-            const data = response.json();
+            const data = await response.json();
             console.log("Restaurant data:", data);
         } else {
             // Handle error when response status is not ok
@@ -17,7 +15,7 @@ async function getRestaurantData() {
 
     } catch (error) {
         // Handle other errors, such as network errors
-        console.error("Error fetching data: ", error.message);
+        console.error("Error fetching data:", error.message);
     }
 } 
 
