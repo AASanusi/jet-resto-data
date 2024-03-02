5d6c6e9abb9a5f923d51fd747ae700c1f4ace635
AASanusi <alaminm.sanusi@gmail.com>
2024-03-02 19:56:46 +0000

    Remove fetch import

--- axios.js
+++ axios.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const express = require('express');
 const app = express();
 const port = 3000;
-const fetch = require('node-fetch');
+
 
 app.use(express.static('public'));
 
