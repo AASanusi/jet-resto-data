4194842b103a13a5e4dcad4e9d5ecb7f04f998be
AASanusi <alaminm.sanusi@gmail.com>
2024-03-02 18:46:31 +0000

    Use axios to request data

--- axios.js
+++ axios.js
@@ -8,9 +8,42 @@ app.use(express.static('public'));
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-  });
+});
 
 
+axios.get("https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/")
+
+    .then(response => {
+
+        if (response.status === 200) {
+            const data = response.data
+            const keys = Object.keys(data)
+
+            const secondKey = keys[1];
+            const restaurants = data[secondKey];
+
+            if (restaurants.length > 0) {
+
+                for (let i = 0; i < 10; i++) {
+                    console.log("Restaurant name: ", restaurants[i].name)
+                    console.log("Cuisines: ", restaurants[i].cuisines)
+                    console.log("Ratings: ", restaurants[i].rating.starRating)
+                    console.log("Address: ", restaurants[i].address)
+                    console.log("---------------------------------------------------------------------")
+                }
+            } else {
+                console.log('There are no results found');
+            }
+
+        } else {
+            console.log(`Failed to fetch data. Status: ${response.status}`)
+        }
+
+    })
+
+    .catch(error => {
+        console.error('Error fetching data:', error.message);
+    });
 
 
 
